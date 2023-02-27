21d9123f19096ef096ff6f656bc4bb2f0bbf45ac
OtaIus <52911203+OtaIus@users.noreply.github.com>
2023-02-27 12:52:10 -0300

    Delete proyecto-final/Curso Youtube directory

--- proyecto-final/Curso Youtube/Wmedia Front-end/parametros-rest.js
+++
@@ -1,7 +0,0 @@
-function checkOut(... prices) {
-    const total = prices.reduce((acc, curr) => acc + curr)
-    console.log(total) // una matriz
-}
-
-checkOut(10, 33, 66)
-checkOut(10, 33, 66, 56, 92)
